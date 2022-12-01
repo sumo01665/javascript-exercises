62dc131a0ee4f2b414920e0f92f2446df239b7f3
Trist T <t35truon@uwaterloo.ca>
2022-12-01 08:19:02 -0500

    exercise02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(str, iter) {
+    let ini_str = str;
+    if (iter == 0) str = ''; 
+    else if (iter < 0) str = 'ERROR';
+    else{
+        for (let count = 0; count < iter - 1; ++count){
+            str += ini_str; 
+        }
+    }
+    console.log(str); 
+    return str; 
 };
+
+repeatString("hey", 3); 
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverse_string, smaller_string = str; 
+    if (str == '') return '';
+    
+    for (let i = 0; i < str.length; ++i){ 
+        reverse_string += smaller_string.slice(-1); 
+        smaller_string = str.slice(0, str.length - 1);  
+        str = smaller_string; 
+    } 
+    console.log(smaller_string);
+    // console.log( reverse_string);
 };
+
+
+reverseString("Hello"); 
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
